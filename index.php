f5dd17e5f0975cc8f1fb0b34bd16836e393be3d4
Bikalpa Pahari <bp359@afsconnect1.njit.edu>
2017-11-05 11:40:06 -0500

    Added Table

--- index.php
+++ index.php
@@ -21,6 +21,45 @@ try{
     $query->execute();
     $output=$query->setFetchMode(PDO::FETCH_ASSOC);
     $output=$query->fetchAll();
-    print_r($output);
+   
 }
+catch (PDOException $error)
+{
+    echo $sql ."<br>".$error->getMessage();
+}
+echo "Records from accounts table ";
+print_r(count($output));
+echo"<br>";
+
+       echo "<table border=1,width=80%,>";
+      	echo '<tr><th>id</th>
+      		<th>email</th>
+          <th>fname</th>
+          <th>lname</th>
+      		<th>phone</th>
+      		<th>birthday</th>
+      		<th>gender</th>
+      		<th>password</th>
+      		</tr>';
+        
+        foreach($output as $row){
+        	echo "<tr><td>".$row["id"].'</td>
+        		<td>'.$row["email"].'</td>
+        		<td>'.$row["fname"].'</td>
+        		<td>'.$row["lname"].'</td>
+        		<td>'.$row["phone"].'</td>
+        		<td>'.$row["birthday"].'</td>
+        		<td>'.$row["gender"].'</td>
+        		<td>'.$row["password"].'</td>';
+
+        		echo'</tr>';
+        		
+        	
+        
+        
+    }
+
+  echo"</table>";
+        
+    
 ?>
