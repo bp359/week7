3d789ba311e26eeb261c18ab3fd1914654db1c93
Bikalpa Pahari <bp359@afsconnect1.njit.edu>
2017-11-04 23:45:51 -0400

    Establishing Connection

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+//error reporting on
+ini_set("display_errors",'On');
+error_reporting(E_ALL);
+
+$hostname ='sql.njit.edu';
+$user="bp359";
+$password="bTDLnIOQ";
+$dsn='mysql:dbname=bp359';
+$connection=NULL;
+try{
+    $connection=new PDO("mysql:host=$hostname;dbname=bp359",$user,$password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo'Connected Successfully'.'<br>';
+}
+catch (PDOException $error){
+    echo'Connection failed'.$error->getMessage().'<br>';
+}
+?>
